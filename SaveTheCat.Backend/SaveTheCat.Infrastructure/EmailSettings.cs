0ab847c8ce425d87eece70814cda45e13d6bf2d4
Simon Holmquist <simonholmquist@outlook.com>
2025-11-27 14:35:25 -0300

    Update default SenderAddress in EmailSettings
    The `SenderAddress` property in the `EmailSettings` class was updated.
    The default value was changed from
    `"no-reply@tudominio.azurecomm.net"` to
    `"DoNotReply@74106ce1-4508-4d4c-bf17-f9f9a8387483.azurecomm.net"`.

--- SaveTheCat.Backend/SaveTheCat.Infrastructure/EmailSettings.cs
+++ SaveTheCat.Backend/SaveTheCat.Infrastructure/EmailSettings.cs
@@ -4,6 +4,6 @@ public class EmailSettings
 {
     // Esta connection string va al Key Vault
     public string CommunicationServiceConnectionString { get; set; } = string.Empty;
-    public string SenderAddress { get; set; } = "no-reply@tudominio.azurecomm.net";
+    public string SenderAddress { get; set; } = "DoNotReply@74106ce1-4508-4d4c-bf17-f9f9a8387483.azurecomm.net";
     public string ClientAppUrl { get; set; } = string.Empty;
 }
